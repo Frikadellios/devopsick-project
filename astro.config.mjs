8b60d3736ec01603b4f5d08d96e4ef43d11b07cd
frikadellios <frikadellios@gmail.com>
2024-07-18 12:57:44 +0000

    +++

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import sitemap from '@astrojs/sitemap'
 import tailwindcss from '@tailwindcss/vite'
 import legacy from '@vitejs/plugin-legacy'
 import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
 import lightningcss from 'vite-plugin-lightningcss'
+import AutoImport from 'unplugin-auto-import/astro'
 // https://astro.build/config
 export default defineConfig({
 	vite: {
@@ -23,6 +25,29 @@ export default defineConfig({
 	prefetch: {
 		prefetchAll: true
 	},
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()]
+	site: 'https://devopsick.com',
+	integrations: [sitemap(),  
+	AutoImport({
+		defaultExportByFilename: false,
+		include: [
+		  /\.[tj]sx?$/ // .ts, .tsx, .js, .jsx
+		],
+		packagePresets: ['detect-browser-es'],
+		imports: ['react', 'react-router'],
+		dirs: ['./src/utils/*.ts', './src/hooks'],
+		dts: './src/auto-imports.d.ts'
+	  }),
+	react({
+		experimentalReactChildren: true
+	  }),
+	mdx({
+		syntaxHighlight: 'shiki',
+		shikiConfig: {
+		  theme: 'github-dark-dimmed',
+		  wrap: true
+		},
+		gfm: true,
+		drafts: true
+	  }),
+	]
 })
