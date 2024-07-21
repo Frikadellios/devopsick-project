0f78ab7c4774b7cdaee8b4378e3d30a751c7d460
Hrihorii Ilin <devopsick@pm.me>
2024-07-21 13:54:43 +0300

    ++

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import mdx from '@astrojs/mdx'
+import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
 import tailwindcss from '@tailwindcss/vite'
 import legacy from '@vitejs/plugin-legacy'
+import icon from 'astro-icon'
 import { defineConfig } from 'astro/config'
-import react from '@astrojs/react'
-import lightningcss from 'vite-plugin-lightningcss'
 import AutoImport from 'unplugin-auto-import/astro'
+import lightningcss from 'vite-plugin-lightningcss'
+
 // https://astro.build/config
 export default defineConfig({
 	vite: {
@@ -26,28 +28,28 @@ export default defineConfig({
 		prefetchAll: true
 	},
 	site: 'https://devopsick.com',
-	integrations: [sitemap(),  
-	AutoImport({
-		defaultExportByFilename: false,
-		include: [
-		  /\.[tj]sx?$/ // .ts, .tsx, .js, .jsx
-		],
-		packagePresets: ['detect-browser-es'],
-		imports: ['react', 'react-router'],
-		dirs: ['./src/utils/*.ts', './src/hooks'],
-		dts: './src/auto-imports.d.ts'
-	  }),
-	react({
-		experimentalReactChildren: true
-	  }),
-	mdx({
-		syntaxHighlight: 'shiki',
-		shikiConfig: {
-		  theme: 'github-dark-dimmed',
-		  wrap: true
-		},
-		gfm: true,
-		drafts: true
-	  }),
+	integrations: [
+		sitemap(),
+		AutoImport({
+			defaultExportByFilename: false,
+			include: [
+				/\.[tj]sx?$/ // .ts, .tsx, .js, .jsx
+			],
+			packagePresets: ['detect-browser-es'],
+			imports: ['react', 'react-router'],
+			dirs: ['./src/utils/*.ts', './src/hooks'],
+			dts: './src/auto-imports.d.ts'
+		}),
+		react({ experimentalReactChildren: true }),
+		mdx({
+			syntaxHighlight: 'shiki',
+			shikiConfig: {
+				theme: 'github-dark-dimmed',
+				wrap: true
+			},
+			gfm: true,
+			drafts: true
+		}),
+		icon()
 	]
 })
